34e32ecefac8a2635d3a78c0b2b8e60bc67e6cfb
Ali <alkhdyyr@gmail.com>
2024-08-24 10:49:14 +0200

    test husk

---
+++ src/components/ui/Sidebar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <div className="w-[300px] min-w-[300px] border-r min-h-screen p-4">
+      side
+    </div>
+  );
+};
+
+export default Sidebar;
