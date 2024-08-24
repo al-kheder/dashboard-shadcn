d1823475eb3a4de5de3574446058f5b81b27dd20
Ali <alkhdyyr@gmail.com>
2024-08-24 17:20:37 +0200

    add ti toturial link

--- src/components/ui/Sidebar.tsx
+++ src/components/ui/Sidebar.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import UserItem from "../UserItem";
 
 const Sidebar = () => {
+    //https://www.youtube.com/watch?v=mju7e6Cf6Nk&list=PL8HkCX2C5h0VGhZnfbwf2hq7yD7nXMbJJ&index=3
   return (
     <div className="flex flex-col gap-4 w-[300px] min-w-[300px] border-r min-h-screen p-4">
       <div>
